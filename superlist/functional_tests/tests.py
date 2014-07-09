13c64d70f85ac5ad26bd592c84878d1a6fc100c5
tdhris <tdhristova@yahoo.com>
2014-07-09 14:29:04 +0300

    made functional tests pass

--- superlist/functional_tests/tests.py
+++ superlist/functional_tests/tests.py
@@ -29,7 +29,7 @@ class TestBasicDjango(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, 'lists/1')
+        self.assertRegex(edith_list_url, 'lists/.+')
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
@@ -49,7 +49,7 @@ class TestBasicDjango(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/lists/2')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         page_text = self.browser.find_element_by_tag_name('body').text
@@ -63,3 +63,22 @@ class TestBasicDjango(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+
+# class NewVisitorTest(LiveServerTestCase):
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
+
+#     def tearDown(self):
+#         self.browser.quit()
+
+#     def test_layout_and_styling(self):
+#         self.browser.get(self.live_server_url)
+#         self.browser.set_window_size(1024, 768)
+
+#         inputbox = self.browser.find_element_by_id('id_new_item')
+#         self.assertAlmostEqual(
+#             inputbox.location['x'] + inputbox.size['width'] / 2,
+#             512,
+#             delta=5
+#         )
--- superlist/lists/models.py
+++ superlist/lists/models.py
@@ -7,4 +7,5 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None)
+    list = models.ForeignKey(List, default=None,
+                             related_name='items')
